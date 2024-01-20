e907a39d54afad1085d473bf50eafc6c6ba61fd1
TahaHaksal <m.haksal@gmail.com>
2024-01-20 22:50:41 +0300

    Completed Timestamps

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +19,45 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isValidDate = (dateStr) => {
+  return !isNaN(new Date(dateStr));
+}
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const dateString = req.params?.date;
+  if (!dateString){
+    const utcDate = new Date().toUTCString();
+    const unixDate = new Date().getTime();
+    res.json({
+      unix: unixDate,
+      utc: utcDate
+    });
+    return ;
+  }
+  const isTimestamp = /^\d+$/.test(dateString);
+  if (isTimestamp){
+    const timestamp = parseInt(dateString, 10);
+    const inputDate = new Date(timestamp);
+    const utcDate = inputDate.toUTCString();
+    const unixDate = inputDate.getTime();
+    res.json({
+      unix: unixDate,
+      utc: utcDate
+    })
+  } else if (isNaN(new Date(dateString))){
+    res.json({
+      error: "Invalid Date"
+    })
+  } else {
+    const inputDate = new Date(dateString);
+    const utcDate = inputDate.toUTCString();
+    const unixDate = inputDate.getTime();
+    res.json({
+      unix: unixDate,
+      utc: utcDate
+    })
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
